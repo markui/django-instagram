880b241824a5e39b1c90ba6afb14c57cc2993e04
Mark Kim <viking617617@gmail.com>
2017-10-13 16:01:19 +0900

    migrated to instagram postgresql DB

--- instagram/config/settings.py
+++ instagram/config/settings.py
@@ -105,12 +105,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = config_secret_common['django']['databases']
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
